0f7cc527a4ad0704b37abbf0f323a8ec257d6b65
Eris Fairbanks <eris.fairbanks@arcturus.studio>
2023-03-02 21:33:40 -0400

    Tweaking and polishing VU code heavily.

--- unfinished/uncertainty_vu/uncertainty_vu.ino
+++ unfinished/uncertainty_vu/uncertainty_vu.ino
@@ -9,9 +9,13 @@
 
 using namespace fp;
 
-// number of gates
+#define TIMER_INTERVAL ((int)(1000000.0/40000.0))
+#define SAMPLE_RATE (1000000.0/TIMER_INTERVAL)
+
+// number of gate outs
 #define NUM_GATES 8
 
+// audio/cv "real" numbers
 typedef fp_t<int,12> cv_t;
 
 // hold pins for gates
@@ -21,12 +25,110 @@ int inputPin = 26;
 // last ADC value read, used to detect rising edge
 cv_t lastInput = cv_t(0.0);
 
+// trigger generator
+class Trigger {
+public:
+  int phase;
+  int output;
+  int width;
+  Trigger(int output=0, int widthInSamples=100) {
+    this->output = output;
+    this->width = widthInSamples;
+    this->phase = widthInSamples+1;
+  }
+  void Reset() {
+    phase = 0;
+  }
+  bool Done() {
+    return phase > width;
+  }
+  void Process() {
+    gpio_put(gatePins[output], phase < width ? 1 : 0);
+    if(phase <= width) phase++;
+  }
+};
+
+class MeterBallistics {
+private:
+  // we're doing filtering of a sort, so let's
+  // use high precision values
+  typedef fp_t<int, 24> slew_t;
+public:
+  slew_t lastVal;
+  MeterBallistics() {
+    lastVal = 0;
+  }
+  cv_t Process(cv_t in) {
+    // get magnitude of input
+    in = in > slew_t(0) ? in : -in;
+    // how much did the magnitude of the input value change since last time?
+    slew_t delta = slew_t(in) - lastVal;
+    // if we're decreasing, seriously limit how quickly we can decrease
+    if(delta < slew_t(0)) {
+      // really hacky but efficient divide by 4096 samples, which is like 100ms fall time
+      lastVal += delta>>12; // modify to alter fall time, bigger number longer fall
+    } else { // otherwise just adjust by the difference, mimicking the input
+      lastVal += delta;
+    }
+    return cv_t(lastVal);
+  }
+};
+
+// cute little led animation
+void startupSequence() {
+  uint32_t startupCounter = 0;
+  // startup sequence
+  while(startupCounter<128*8) {
+    int pinIndex = startupCounter>>7;
+    for(int i=0;i<8;i++) {
+      if(i==pinIndex) {
+        gpio_put(gatePins[pinIndex], startupCounter%128 < 120);
+      } else {
+        gpio_put(gatePins[i], 0);
+      }
+    }
+    startupCounter++;
+    delay(1);
+  }
+}
+
+// classes for handling gate/trigger output
+Trigger* triggers[NUM_GATES];
+MeterBallistics meter;
+
+cv_t debugVal = 0;
+
+// audio rate callback- meat of the program goes here
+static bool audioHandler(struct repeating_timer *t) {
+  // poll ADC and convert from 12 bit value to [-1.0, 1.0]
+  cv_t input = (cv_t(adc_read() - (1<<11)) >> 11) * cv_t(5.0 / 4.42) - cv_t(0.04);
+
+  debugVal = input;
+
+  // meter processing
+  input = meter.Process(input);
+
+  // scale output levels to go from 0 to 8
+  int level = int(input*fp_t<int,0>(8));
+
+  // set outputs
+  for(int i=0;i<8;i++) {
+    if(i+1 <= level) triggers[i]->Reset();
+    triggers[i]->Process();
+  }
+
+  // record the current input for the next calc
+  lastInput = input;
+
+  return true;
+}
+
+struct repeating_timer _timer_;
+
 void setup() {
   // 2x overclock for MAX POWER
-  //set_sys_clock_khz(250000, true);
+  set_sys_clock_khz(250000, true);
 
-  //Serial.begin(115200);
-  
   // initialize ADC
   adc_init();
   adc_gpio_init(inputPin);
@@ -37,32 +139,20 @@ void setup() {
     int pin = gatePins[i];
     gpio_init(pin);
     gpio_set_dir(pin, GPIO_OUT);
+    triggers[i] = new Trigger(i, 500 /* modify/increase to hold gates open for longer */ );
   }
+
+  // oooh pretty
+  startupSequence();
+
+  // audio callback
+  add_repeating_timer_us(-TIMER_INTERVAL, audioHandler, NULL, &_timer_);
+
+  // init serial debugging
+  Serial.begin(9600);
 }
 
 void loop() {
-  // poll ADC as fast as possible
-  cv_t input = cv_t(adc_read() - (1<<11));
-  bool polarity = input > cv_t(0.0) ? true : false;
-  input = polarity ? input : -input;
-
-  // hacky slew limit
-  input = cv_t(input>>10) + cv_t((lastInput>>10)*fp_t<int,0>(1023));
-
-  // scale output levels
-  int level = int((input>>11)*fp_t<int,0>(10));
-
-  // set outputs
-  for(int i=0;i<8;i++) gpio_put(gatePins[i], i+1 <= level);
-
-  // record the current input for the next calc
-  lastInput = input;
-
-  /*
-  Serial.printf("Input: %f\n", float(input));
-  Serial.printf("Norml: %f\n", float(input>>11));
-  Serial.printf("Intgr: %d\n", int((input>>11)*fp_t<int,0>(9)));
-
+  Serial.println(float(debugVal));
   delay(100);
-  */
 }
